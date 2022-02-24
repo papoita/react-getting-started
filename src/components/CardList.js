4a5c697d3c6bfcb8823cae3a662f6347089a146a
PaolaP <paolaperez8@gmail.com>
2022-02-24 17:15:22 -0500

    load fetchData automatically and add gif for loading

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 
 function CardList() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = () => {
-    const API_URL = "https://pokeapi.co/api/v2/pokemon";
+    setLoading(true)
+    setTimeout(() => {
+      const API_URL = "https://pokeapi.co/api/v2/pokemon";
     fetch(API_URL)
       .then((response) => response.json())
-      .then((json) => setData(json.results))
+      .then((json) => {
+        setData(json.results)
+        setLoading(false)
+      })
       .catch((error) => console.log(error));
+    }, 3000);
   };
 
   const renderedData = data.map((element) => (
     <Card key={element.name} name={element.name} url={element.url} />
   ));
 
+  /* useEffect (callback, [dependecies]) */
+  useEffect(() => fetchData() , [])
+
+  if (loading){
+    return (
+      <div className="loading"> <img src="https://i.gifer.com/Yg6z.gif" alt="loading gif"/> </div>
+    )
+  }
   return (
     <div className="pokemon-list">
-      <button onClick={fetchData} className="pokemon-button"> fetch pokemon </button>
+      { /* <button onClick={fetchData} className="pokemon-button"> fetch pokemon </button> */}
       <Card
         name="Pikachu"
         type="electricidad"
